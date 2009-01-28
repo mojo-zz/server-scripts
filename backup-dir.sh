95e05477394851217a71e496265f0ac6b9cce55f
Sam Stokes <sam@box.mo.jo>
2009-01-28 18:39:06 +0000

    generalise backup-wpuploads.sh into backup-dir.sh

--- backup-dir.sh
+++ backup-dir.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-SRC_DIR=/var/www/ovcblog/shared/uploads
-BACKUP_DIR=/home/backup/wpuploads
 BACKUP_BASENAME=backup
 MAX_BACKUPS=14
 S3_BUCKET=ovc-backup
-S3_PREFIX=wpuploads
 
 error() {
   echo "$@" >&2
   exit 1
 }
 
-backup_prefix="$BACKUP_DIR"/"$BACKUP_BASENAME"
+
+src_dir="${1?First argument must be the directory to backup}"
+test -d "$src_dir" || error "$src_dir is not a directory"
+backup_dir="${2?Second argument must be path to backup directory}"
+test -d "$backup_dir" || error "$backup_dir is not a directory"
+s3_prefix="${3?Third argument must be S3 prefix in which to store backups}"
+
+backup_prefix="$backup_dir"/"$BACKUP_BASENAME"
 
 timestamp=`date +%Y%m%d%H%M%S` || error "Failed to compute timestamp"
 newbackup="$backup_prefix"."$timestamp"
-prevbackup=`ls -c "$BACKUP_DIR"/ | head -1`
-prevbackup_full="$BACKUP_DIR"/"$prevbackup"
+prevbackup=`ls -c "$backup_dir"/ | head -1`
+prevbackup_full="$backup_dir"/"$prevbackup"
 
 rsync_opts="-a --delete"
 
@@ -32,28 +36,29 @@ else
   rsync_opts="$rsync_opts --link-dest=$prevbackup_full"
 fi
 
-rsync $rsync_opts "$SRC_DIR" "$newbackup" \
+rsync $rsync_opts "$src_dir" "$newbackup" \
   || error "Unable to create new backup $newbackup"
 echo Created new backup "$newbackup".
 
 # delete old backups if there are too many
-current=`find "$BACKUP_DIR" -type d -name "$BACKUP_BASENAME.*" | wc -l` \
+current=`find "$backup_dir" -type d -name "$BACKUP_BASENAME.*" | wc -l` \
   || error "Failed to check number of current backups"
 excess=$[current - MAX_BACKUPS]
 echo Out of max $MAX_BACKUPS backups we have $current.
 if [ $excess -gt 0 ]; then
   echo Deleting $excess oldest backups.
-  pushd "$BACKUP_DIR" >/dev/null # shush
+  pushd "$backup_dir" >/dev/null # shush
   ls -cr | head -$excess | xargs rm -rf \
     || { popd >/dev/null; error "Failed to delete $excess oldest backups"; }
   popd >/dev/null
 fi
 
-echo Total disk space in bytes occupied by wpuploads backups: `du -hs "$BACKUP_DIR" | awk '{print $1}'`
+echo -n Total disk space in bytes occupied by backups of "$src_dir":
+echo    `du -hs "$backup_dir" | awk '{print $1}'`
 
 echo Syncing to Amazon S3...
 # no --delete here, may as well keep older backups
-s3sync --ssl --recursive "$BACKUP_DIR"/ "$S3_BUCKET":"$S3_PREFIX" \
-  || error "Failed to sync to S3 ($BACKUP_DIR/ $S3_BUCKET:$S3_PREFIX)"
+s3sync --ssl --recursive "$backup_dir"/ "$S3_BUCKET":"$s3_prefix" \
+  || error "Failed to sync to S3 ($backup_dir/ $S3_BUCKET:$s3_prefix)"
 
 echo Backup complete.
